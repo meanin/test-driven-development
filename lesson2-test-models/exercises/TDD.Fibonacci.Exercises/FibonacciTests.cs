45167efb72bfcb54c9ad2de1937d9a8ff67b0ccc
Paweł Ruciński <pawel.rucinski@cybercom.com>
2017-10-04 10:52:14 +0200

    Renames, cleanup and added some exercises

---
+++ lesson2-test-models/exercises/TDD.Fibonacci.Exercises/FibonacciTests.cs
@@ -0,0 +1,43 @@
+﻿namespace TDD.Fibonacci.Exercises
+{
+    /// <summary>
+    /// This file exposes basic exercise for creating your first tests
+    /// 
+    /// Few step to follow:
+    /// 1. Choose your favorite test engine and download it as nuget package
+    /// 2. Add reference to project TDD.Fibonacci
+    /// 3. Select your favourite test models nuget
+    /// 4. Write your first test
+    ///     Remember test first
+    /// 5. Write only that code that makes tests green
+    /// </summary>
+    /// 
+    /// MSTest
+    /// [TestClass]
+    public class FibonacciTests
+    {
+        // MSTest
+        // [TestMethod]
+
+        // NUnit
+        //[Test]
+
+        // XUnit
+        //[Fact]
+        public void GetValue_ShouldNotThrow()
+        {
+            // arrange
+
+            // initialize here new instance of tested class ( new Fibonacci() )
+            // initialize input data from test model nuget
+
+            // act
+
+            // run tested method and get output
+
+            // assert
+
+            // Validate that method not throw
+        }
+    }
+}
--- lesson2-test-models/exercises/TDD.Fibonacci.Exercises/Properties/AssemblyInfo.cs
+++ lesson2-test-models/exercises/TDD.Fibonacci.Exercises/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TDD.Classes")]
+[assembly: AssemblyTitle("TDD.Fibonacci.Exercises")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TDD.Classes")]
+[assembly: AssemblyProduct("TDD.Fibonacci.Exercises")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e0e1af93-2841-4d62-b468-8ca9bc73c7cb")]
+[assembly: Guid("13a4a2eb-a23a-44a5-9922-f4d8202bab1b")]
 
 // Version information for an assembly consists of the following four values:
 //
